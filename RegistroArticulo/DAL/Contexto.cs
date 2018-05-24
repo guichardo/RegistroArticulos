a7cf5fb33fcfba9a25aa3e7428ca5d93937f37d2
guichardo <guichardo2@outlook.com>
2018-05-23 22:24:23 -0400

    Acabe el contexto

---
+++ RegistroArticulo/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using RegistroArticulo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroArticulo.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        public Contexto() : base("ConStr") { }
+    }
+}
